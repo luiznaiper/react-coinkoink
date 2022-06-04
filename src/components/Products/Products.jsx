02e3b707116a3390b91d7c1c3413d86a72d9efe5
luiznaiper <luiz_naiper@hotmail.com>
2022-06-04 00:32:38 -0500

    :construction: Added navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { Product } from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
   {
@@ -19,8 +20,10 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
