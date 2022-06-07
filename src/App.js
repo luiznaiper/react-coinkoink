a713a758fd4964e710619d871a55984b8f835fd3
luiznaiper <luiz_naiper@hotmail.com>
2022-06-07 13:41:39 -0500

    :construction: Added fetch for products

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {commerce} from "./lib/commerce"
 import { Products } from './components/Products/Products'
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async() => {
+    const {data} = await commerce.products.list()
+    setProducts(data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  console.log(products)
   return (
     <div>
         <Products />
