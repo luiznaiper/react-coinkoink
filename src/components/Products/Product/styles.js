70714342db236a7cb4a97b0c1b45e8abe052b0c0
luiznaiper <luiz_naiper@hotmail.com>
2022-06-16 14:15:50 -0500

    :lipstick: Making better the porks, added 2 more to the api

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -5,7 +5,7 @@ export default makeStyles(() => ({
         maxWidth: "100%",        
     },
     media: {
-        height: 100,
+        height: 200,
         paddingTop: "56.25%",
     },
     cardActions: {
