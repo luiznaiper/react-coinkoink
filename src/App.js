250062812c8b3cd7d9154395db1fa6d58b9aa167
luiznaiper <luiz_naiper@hotmail.com>
2022-06-17 21:03:33 -0500

    :construction: Add increase, decrease and remove from cart

--- src/App.js
+++ src/App.js
@@ -22,9 +22,24 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity)
+    const {cart} = await commerce.cart.add(productId, quantity)
   
-    setCart(item.cart)
+    setCart(cart)
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const {cart} = await commerce.cart.update(productId, {quantity})
+    setCart(cart)
+  }
+
+  const handleRemoveFromCart = async (productId, quantity) => {
+    const {cart} = await commerce.cart.remove(productId, {quantity})
+    setCart(cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const {cart} = await commerce.cart.empty()
+    setCart(cart)
   }
 
   useEffect(() => {
@@ -40,7 +55,15 @@ const App = () => {
           <Navbar totalItems={cart.total_items}/>
           <Routes>
             <Route path='/' element={<Products products = {products} onAddToCart = {handleAddToCart}/>} />
-            <Route path='/carrito' element={<Cart cart={cart} />} />
+            <Route path='/carrito' element={
+              <Cart
+              cart={cart} 
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+              />
+            } 
+            />
           </Routes>          
         </div>
     </Router>
