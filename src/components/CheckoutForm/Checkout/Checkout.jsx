cdde016f81e7669d44b6419a0384c84bf232ef9f
luiznaiper <luiz_naiper@hotmail.com>
2022-07-01 15:07:59 -0500

    :construction: Review added succesfull

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -18,7 +18,6 @@ const Checkout = ({cart}) => {
       const generateToken = async () => {
         try {
           const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
-          console.log(token);
           setCheckoutToken(token);
         } catch {
           //if (activeStep !== steps.length) history.push('/');
@@ -46,7 +45,7 @@ const Checkout = ({cart}) => {
 
   const Form= () => activeStep === 0
     ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-    : <PaymentForm shippingData={shippingData} />
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken}/>
   return (
     <React.Fragment>
         <div className={classes.toolbar}>
