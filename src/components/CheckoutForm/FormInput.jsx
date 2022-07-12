7ad8d03e9570a235662362d73286009fadd5ea6d
luiznaiper <luiz_naiper@hotmail.com>
2022-07-12 12:54:33 -0500

    :bug: Add render in Controller component

--- src/components/CheckoutForm/FormInput.jsx
+++ src/components/CheckoutForm/FormInput.jsx
@@ -4,16 +4,23 @@ import { useFormContext, Controller } from 'react-hook-form'
 
 const FormInput = ({name, label}) => {
     const {control} = useFormContext()
+    const isError = false;
   return (
     <Grid item xs={12} sm={6}>
       <Controller
-        as={TextField}
-        defaultValue=""
+        render = {()=> (
+                <TextField
+                    fullWidth
+                    label={label}
+                    required
+                />
+            )}
         control={control}
         fullWidth
         name={name}
         label={label}
         required
+        error={isError}
       />
     </Grid>
   )
