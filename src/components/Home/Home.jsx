87606279640a8c53c48bbc501b3ed55f554fcf16
luiznaiper <luiz_naiper@hotmail.com>
2022-07-19 14:35:45 -0500

    :construction: Add some components to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  Typography,
+  Button,
+  CssBaseline,
+  Grid,
+  Container,
+} from "@material-ui/core";
+import { Navbar } from "../Navbar/Navbar";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <Typography variant="h1">Estás en Coink Oink</Typography>
+      <Typography variant="h4">
+        Las Manos del Artesano Exploran el Infinito
+      </Typography>
+    </>
+  );
 };
 
 export { Home };
